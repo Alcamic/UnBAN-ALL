8ab544fac5cc53e3df9acfa2863feb7674175b12
raibonn <66820348+raibonn@users.noreply.github.com>
2022-10-01 17:08:39 +0900

    Update unbanall.py

--- unbanall.py
+++ unbanall.py
@@ -1,14 +1,15 @@
 import discord
+import sys
 from discord.ext import commands
 intents=discord.Intents.all()
 bot = commands.Bot(command_prefix = "ctkp!", intents=intents)
-Token = ''
+Token = sys.argv[0]
 
 @bot.event
 async def on_ready():
     print("準備完了")
-    target = bot.get_guild(867341022937612288)
-    async for entry in target.bans(limit=20000):
+    target = bot.get_guild(sys.argv[1])
+    async for entry in target.bans(limit=sys.argv[2]):
         print(entry.user)
         await target.unban(entry.user)
 
